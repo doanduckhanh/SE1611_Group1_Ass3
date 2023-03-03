a959baab415ce35a762f4667ce845724864fa054
Nguyễn Tiến Nhất <nhatnthe153132@fpt.edu.vn>
2023-03-03 11:47:43 +0700

    Checkout

---
+++ SE1611_Group1_A3/SE1611_Group1_A3/Pages/Shopping/Checkout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SE1611_Group1_A3.Pages.Shopping
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
