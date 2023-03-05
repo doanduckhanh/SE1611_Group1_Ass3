c7af14900ebaf79a77ab6c93ab0fc90a09d83993
khanhddhe150956 <khanhddhe150956@fpt.edu.vn>
2023-03-05 16:34:56 +0700

    Update

---
+++ SE1611_Group1_A3/SE1611_Group1_A3/Pages/Genres/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SE1611_Group1_A3.Models;
+
+namespace SE1611_Group1_A3.Pages.Genres
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SE1611_Group1_A3.Models.MusicStoreContext _context;
+
+        public CreateModel(SE1611_Group1_A3.Models.MusicStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Genre Genre { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Genres.Add(Genre);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ SE1611_Group1_A3/SE1611_Group1_A3/Pages/Genres/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SE1611_Group1_A3.Models;
+
+namespace SE1611_Group1_A3.Pages.Genres
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SE1611_Group1_A3.Models.MusicStoreContext _context;
+
+        public DetailsModel(SE1611_Group1_A3.Models.MusicStoreContext context)
+        {
+            _context = context;
+        }
+
+      public Genre Genre { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres.FirstOrDefaultAsync(m => m.GenreId == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Genre = genre;
+            }
+            return Page();
+        }
+    }
+}
